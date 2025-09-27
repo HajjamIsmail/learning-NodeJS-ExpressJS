7a67d65e1a849ee38d507d885e10cd0eccb3bc1d
HAJJAM ISMAIL <hajjam.ism@gmail.com>
2025-09-27 12:37:43 +0100

    #feat: adding authentification

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 // calls routers
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://ismo01:1234@cluster0.rvscyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   { useNewUrlParser: true,
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
